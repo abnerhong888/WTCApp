e33379fda01f5d129f87c4bb611d30aa6f056263
abnerhong888 <abnerhong@example.com>
2025-09-26 21:44:01 +0800

    main.cpp example of gtk4

--- WTCClient/main.cpp
+++ WTCClient/main.cpp
@@ -4,13 +4,28 @@
 
 #include "UDPClient.h"
 
-int main(void){	
-	wtc::UDPClient client;
-	int cnt = 0;
-	while(1){
-		client.send(cnt);
-		++cnt;
-		au::sleep_ms(1000);
-	}
-	return 0;
+#include <gtk/gtk.h>
+
+static void on_activate(GtkApplication *app, gpointer user_data) {
+    GtkWidget *window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW(window), "wtc");
+    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+    gtk_window_present(GTK_WINDOW(window));
+	
+}
+
+int main(int argc, char **argv){	
+	GtkApplication *app = gtk_application_new("abner.wtc", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref(app);
+    return status;
+// 	wtc::UDPClient client;
+// 	int cnt = 0;
+// 	while(1){
+// 		client.send(cnt);
+// 		++cnt;
+// 		au::sleep_ms(1000);
+// 	}
+// 	return 0;
 }
