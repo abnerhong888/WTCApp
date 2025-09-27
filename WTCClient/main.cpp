68c687449f2bd1e24aa30e8744a9618de2d6c5d5
abnerhong888 <abnerhong@example.com>
2025-09-27 17:42:13 +0800

    update

--- WTCClient/main.cpp
+++ WTCClient/main.cpp
@@ -5,13 +5,31 @@
 #include "UDPClient.h"
 
 #include <gtk/gtk.h>
+#include "gio/gio.h"
+#include "glib-object.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef DEBUG
+#include "wtcResource.h"
+#endif
+#ifdef __cplusplus
+}
+#endif
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "wtc");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 200);
+    GtkBuilder *builder = gtk_builder_new ();
+#ifdef DEBUG
+    gtk_builder_add_from_file(builder, "/home/user/ws/mygit/WTCApp/WTCClient/resources/ui/main_window.ui", NULL);
+#else
+    gtk_builder_add_from_resource(builder, "/abner/wtc/resources/ui/main_window.ui", NULL);
+#endif
+    GObject *window = gtk_builder_get_object (builder, "main_window");
+
+    gtk_window_set_application (GTK_WINDOW (window), app);
     gtk_window_present(GTK_WINDOW(window));
-	
 }
 
 int main(int argc, char **argv){	
